28a98e9635d8a9bbbf32173e96116e64c7f2a979
kanton99 <avolkov99@gmail.com>
2023-08-18 18:20:49 +0200

    reverted because of issues

--- App/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "SmartAgricultureSystem": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
